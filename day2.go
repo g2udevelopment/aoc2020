40ee7b2a0144b1c93204307745d43d78b0a989c1
Gerben van de Wiel <clshannon@protonmail.com>
2020-12-02 08:51:42 +0100

    Day2

---
+++ day2.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Policy struct {
+	minOcc   int
+	maxOcc   int
+	letter   string
+	password string
+}
+
+// ISValid checks is policy is valid
+func (p Policy) IsValid() bool {
+	count := strings.Count(p.password, p.letter)
+	return count >= p.minOcc && count <= p.maxOcc
+}
+
+func (p Policy) IsValidSecondPolicy() bool {
+	valid := false
+	for idx, letter := range p.password {
+		tdx := idx + 1
+		if string(letter) == p.letter {
+			if !valid && (tdx == p.minOcc || tdx == p.maxOcc) {
+				valid = true
+			} else if valid && (tdx == p.maxOcc) {
+				valid = false
+				break
+			}
+
+		}
+	}
+	return valid
+}
+
+func main() {
+	lines, _ := readLines("./input-day2")
+	var count1 int
+	var count2 int
+	for _, line := range lines {
+		pol := parseLine(line)
+		if pol.IsValid() {
+			count1++
+		}
+		if pol.IsValidSecondPolicy() {
+			count2++
+		}
+	}
+	fmt.Printf("Number of valid passwords 1st pol: %v, 2nd pol: %v", count1, count2)
+}
+
+func parseLine(line string) Policy {
+	split := strings.Split(line, " ")
+	boundary := strings.Split(split[0], "-")
+	min, _ := strconv.Atoi(boundary[0])
+	max, _ := strconv.Atoi(boundary[1])
+	letter := split[1][0] //it is ascii so don't bother with runes
+	pwd := split[2]
+	return Policy{minOcc: min, maxOcc: max, letter: string(letter), password: pwd}
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
